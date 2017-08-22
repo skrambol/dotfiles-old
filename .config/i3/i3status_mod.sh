ebaa6f5f3a818a7311e6d67eada107cd7ac3cfd1
systemplado <systemplado@gmail.com>
2017-08-22 22:50:55 +0800

    cmus/cava integration

--- .config/i3/i3status_mod.sh
+++ .config/i3/i3status_mod.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/zsh
 
 i3status -c ~/.config/i3/i3status.conf | while :
 do
   read line
-  #artist=$(cmus-remote -Q | grep ' artist ' | cut -d ' ' -f3-)
-  #song=$(cmus-remote -Q | grep title | cut -d ' ' -f3-)
-  artist=$( playerctl metadata artist )
-  song=$( playerctl metadata title )
+
+  if [[ $(cmus-remote -Q | awk 'NR==1{print $2}') == "playing" ]]; then
+    artist=$(cmus-remote -Q | grep 'tag artist ' | cut -d ' ' -f3-)
+    song=$(cmus-remote -Q | grep 'tag title ' | cut -d ' ' -f3-)
+  else
+    artist=$( playerctl metadata artist )
+    song=$( playerctl metadata title )
+  fi
+
   echo "$artist - $song | $line" || exit 1
 done
